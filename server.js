fa25b65464ddf00e3fee8dbe48c5e6e38da3f62a
papain26 <sy4422.b@gmail.com>
2026-03-08 19:39:46 +0900

    fix server

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ const axios = require("axios");
 
 const app = express();
 
-// CORS 설정
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Methods", "GET, POST, DELETE, OPTIONS");
